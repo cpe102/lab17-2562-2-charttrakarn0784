cc98fc70302243feb2dbc15125b696db1b77f9bc
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2020-03-17 17:42:53 +0700

    lab17

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -13,11 +13,27 @@ string checkGrade(int g)
 	else if(g >= 50) return "D";
 	else return "F";
 }
-void makeLine()
-	{
-		for(int i=0;i<20;i++) cout <<"-";
-		cout << "\n";
+bool checkInput(string s)
+{
+	string grade[5] = {"A","B","C","D","F"};
+	for(int i=0;i<5;i++) {
+		if(s == grade[i]) return true;
 	}
+	return false;
+}
+void makeLine()
+{
+		for(int i=0;i<30;i++) cout <<"-";
+		cout << "\n";
+}
+string Upper(string s)
+{
+	string s_new;
+	for(int i=0;i<s.size();i++) {
+		s_new += toupper(s[i]);
+	}
+	return s_new;
+}
 int main() {
 	vector<string> v_name,v_grade;
 	ifstream f("name_score.txt");
@@ -33,25 +49,41 @@ int main() {
 	}
 while(true){
 	cout << "Please input your command: ";
-	getline(cin,s);
-	int idx = s.find_first_of(" ");
-	string s1 = upper( s.substr(0,idx) ); //Command: name,grade,exit
-	string s2 = upper( s.substr(idx+1,s.size())); //Option: a,b,c,...,Sanji
+	getline(cin,s); //? s = score A, s = name sanji, s = exit
+	int idx = s.find_first_of(" "); //TODO find NAME,EXIT,SCORE
+	string s1 = Upper(s.substr(0,idx)); //Command: name,grade,exit
+	string s2 = Upper(s.substr(idx+1,s.size())); //Option: a,b,c,...,Sanji
+	
 	if(s1 == "NAME")
 	{
+		bool checkFound = false;
 		for(int i=0;i<v_name.size();i++)
 		{
-			if(s2 == upper(v_name[i]))
+			if(s2 == Upper(v_name[i])){
+				makeLine();
 				cout << v_name[i] << "'s grade = " << v_grade[i] <<"\n";
+				checkFound = true;
+			}
 		}
-		makeLine();
+		if(checkFound == false) cout << "Cannot found.\n";
+		else makeLine();
 	}
 	else if(s1 == "GRADE")
 	{
-		
+		bool check = checkInput(Upper(s2));
+		if(check == true){
+			makeLine();
+			for(int i=0;i<v_name.size();i++)
+			{
+				if(Upper(s2) == Upper(v_grade[i]))
+					cout << v_name[i] << "\n";
+			}
+			makeLine();
+		}
+		else cout << "Cannot found.\n";
 	}
 	else if(s1 == "EXIT") break;
-	else cout << "Invalid command."; 
+	else cout << "Invalid command.\n"; 
 	}
 }
 
